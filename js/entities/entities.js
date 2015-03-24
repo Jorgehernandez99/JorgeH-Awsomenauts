062496d7708fceb596dc0438e22a9ee329d50068
Jorge Hernandez <jorgeh905187@gmail.com>
2015-03-24 11:20:26 -0700

    added creep onto the game
    added the image of the creep with movement onto my awsomenauts game for my computer science class

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -205,7 +205,7 @@ game.EnemyCreep = me.Entity.extend({
         this.health = 10;
         this.alwaysUpdate = true;
         
-        this.setVelocity(3, 20);
+        this.body.setVelocity(3, 20);
         
         this.type = "EnemyCreep";
         
@@ -214,7 +214,38 @@ game.EnemyCreep = me.Entity.extend({
         
     },
     
-    update: function() {
+    update: function(delta) {
         
+        
+        this.body.vel.x -= this.body.accel.x * me.timer.tick;
+        
+        this.body.update(delta);
+        
+        
+        
+        this._super(me.Entity, "update", [delta]);
+        
+        return true;
+    }
+});
+
+game.GameManager = Object.extend({
+    init: function(x, y, settings){
+        this.now = new Date().getTime();
+        this.lastCreep = new Date().getTime();
+        
+        this.alwaysUpdate = true;
+    },
+    
+    update: function(){
+        this.now = new Date().getTime();
+        
+        if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)){
+            this.lastCreep = this.now;
+            var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+            me.game.world.addChild(creepe, 5);
+        }
+        
+        return true;
     }
 });
